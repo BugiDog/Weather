b555b411d274e7108d211da6acc01bf9ded4bbff
Sasha Abramov <sancha0809@inbox.ru>
2020-08-09 15:21:27 +0300

    Get weather now

--- routes/weather.js
+++ routes/weather.js
@@ -27,24 +27,13 @@ router.get('/Current', async (req, res) => {
 
 router.get('/OneHour', async (req, res) => {
     try {
-        console.log('req:' + req.query)
-
         const lat = req.query.lat//req.query.lat   33.441792
         const lon = req.query.lon// req.query.lon   -94.037689
-        const part = 'current,hourly,daily'//current   minutely hourly   daily
-        
+        const part = ''//current   minutely hourly   daily
 
-        console.log('1')
-        console.log('config.WeatherKeyAPI:' + config.WeatherKeyAPI)
+        const URL=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${config.WeatherKeyAPI}`
 
-        const URL=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&
-        exclude=${part}&appid=${config.WeatherKeyAPI}`
-       // const URL = `https://https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude =${part}&appid=${config.WeatherKeyAPI}`
         request(URL, (error, response) => {
-            console.log('2')
-            console.log(response.body)
-            console.log(typeof (response))
-            console.log(error)
             res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
             res.json(JSON.parse(response.body))
         })
